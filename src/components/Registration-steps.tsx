7e4db2ba69595011c948db95abcc950d337913a0
unknown <ilia.chkoidze12@gmail.com>
2023-06-29 16:21:36 +0400

    chang bg img url

--- src/components/Registration-steps.tsx
+++ src/components/Registration-steps.tsx
@@ -1,9 +1,10 @@
 import { useLocation } from "react-router-dom";
+import Bg from "../assets/images/bg-sidebar-mobile.svg";
 
 const RegistrationSteps = () => {
   const location = useLocation();
   return (
-    <div className="h-[172px] bg-[url('./src/assets/images/bg-sidebar-mobile.svg')] bg-cover bg-no-repeat">
+    <div className="h-[172px] bg-[url('../src/assets/images/bg-sidebar-mobile.svg')] bg-cover bg-no-repeat">
       <ul className="flex gap-4 justify-center font-ubuntu font-bold text-sm/[16px] tracking[1px] uppercase pt-8">
         <li
           className={`w-[33px] h-[33px] flex items-center justify-center border border-white rounded-[50%] ${
